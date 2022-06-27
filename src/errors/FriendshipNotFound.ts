24df6b6b2a867c8d573188408523e0bdf8e078e2
Matheus Ribeiro <matheuslimaribeiro15@outlook.com>
2022-06-27 08:14:54 -0300

    deploy

--- src/errors/FriendshipNotFound.ts
+++
@@ -1,7 +0,0 @@
-import { CustomError } from './CustomError'
-
-export class FriendshipNotFound extends CustomError {
-  constructor() {
-    super('Friendship not found', 400)
-  }
-}
--- src/errors/PostNotFound.ts
+++
@@ -1,7 +0,0 @@
-import { CustomError } from './CustomError'
-
-export class PostNotFound extends CustomError {
-  constructor() {
-    super('Post not found', 404)
-  }
-}
